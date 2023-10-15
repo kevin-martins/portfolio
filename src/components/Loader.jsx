4f95885090bbdfe0673c0681644737e721af0b75
Kevin <kev.martinsvieira@gmail.com>
2023-10-15 03:16:06 +0200

    feat: component to load model in the scene

---
+++ src/components/Loader.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { useGLTF, Environment } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+// type Props = {
+//   url: string
+//   position?: number[]
+//   rotation?: number[]
+//   scale?: number[]
+// }
+
+const Loader = ({ url, ...props }) => {
+  const levitate = useRef()
+  const model = useGLTF(url)
+  console.log(model)
+  const [hovered, setHovered] = useState(false);
+
+  useEffect(() => void (document.body.style.cursor = hovered ? 'pointer' : 'default'), [hovered])
+
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    levitate.current.rotation.x = THREE.MathUtils.lerp(levitate.current.rotation.x, Math.cos(t) / 20.25, 0.1)
+    // levitate.current.rotation.y = THREE.MathUtils.lerp(levitate.current.rotation.y, Math.sin(t) / 20, 0.1)
+    levitate.current.rotation.z = THREE.MathUtils.lerp(levitate.current.rotation.z, Math.sin(t) / 15, 0.1)
+    // levitate.current.position.y = THREE.MathUtils.lerp(levitate.current.position.y, (-2 + Math.sin(t)) / 20, 0.1)
+  })
+  // onPointerOver={(e) => (e.stopPropagation(), setHovered(true))}
+  return (
+    <mesh ref={levitate} onPointerOver={(e) => (e.stopPropagation(), setHovered(true))}>
+      <primitive object={model.scene} {...props} />
+      <Environment preset='city' />
+    </mesh>
+  )
+}
+
+export default Loader
